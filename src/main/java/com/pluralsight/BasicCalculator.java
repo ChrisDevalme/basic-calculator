3f3aaa01d870e6affafac618893ca9f1876b532b
Chris Devalme <cdevalme@gmail.com>
2026-04-08 14:51:19 -0400

    Updated result variable and changed output

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -11,11 +11,12 @@ public class BasicCalculator {
         System.out.println("Enter your Second number: ");
         double num2 = myScanner.nextDouble();
         myScanner.nextLine();
+        double result = num1 * num2;
 
         System.out.println("Please select an option (A), (S), (M), (D) ");
         String selection = myScanner.nextLine();
 
-        System.out.println("Your numbers multiplied equals: " + (num1 * num2));
+        System.out.println(num1 + " * " + num2 + " = " + result);
         myScanner.close();
     }
 }
