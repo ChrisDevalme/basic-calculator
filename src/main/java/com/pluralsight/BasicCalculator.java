355cb6ecf4f7d0b323e89ada627334e9119515bc
Chris Devalme <cdevalme@gmail.com>
2026-04-08 14:30:19 -0400

    Completed Assignment

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
+        double num1 = myScanner.nextDouble();
+
+        System.out.println("Enter your Second number: ");
+        double num2 = myScanner.nextDouble();
+        myScanner.nextLine();
+
+        System.out.println("Please select an option (A), (S), (M), (D) ");
+        String selection = myScanner.nextLine();
+
+        System.out.println("Your numbers multiplied equals: " + (num1 * num2));
+    }
 }
